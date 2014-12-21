07b8a118b5ad505500bfb483c401684293859999
Alexandr Nigay <nigai.alex@gmail.com>
2014-12-21 22:18:55 +0600

    Changed LPM_AND implementation. Instantiation via constructor; swapped dims and coords in getters return values.

--- edif2ww/wireworld_lpm_tile6.py
+++ edif2ww/wireworld_lpm_tile6.py
@@ -1,6 +1,6 @@
 '''
     EDIF2WW project file.
-    Contains WireWorld LPM cells for the Tile algorithm of size 6.
+    Contains WireWorld LPM cells for the Tile Algorithm of size 6.
     Gates are communicated in the format:
     SPACE   empty
     H       electron head
@@ -23,26 +23,47 @@ class LPM_AND:
         '   C           C  ',
         '                  '
     ]
+    
+    _LPM_instance_name = ''
+    
+    def __init__(self, instance_name, LPM_SIZE, LPM_WIDTH):
+        self._LPM_instance_name = instance_name
+        if (LPM_SIZE != 2 or LPM_WIDTH != 1):
+            raise RuntimeError('Currently LPM_AND supports only LPM_SIZE=2 and LPM_WIDTH=1')
+    
     def get_pattern(self):
         return self._pattern
+        
     def get_delay(self):
+        ''' ... in WW generations '''
         return 24
-    def get_tile_size(self):
-        return (3, 2)
-    def get_cell_size(self):
-        return (18, 12)
+        
+    def get_size_in_tiles(self):
+        ''' ... of size 6 '''
+        return (2, 3)
+        
+    def get_size_in_cells(self):
+        return (12, 18)
+        
     def get_port_local_pos(self, port):
-        ''' Ports location is given in WW cell coordinate space inside gate pattern '''
+        ''' Ports' locations are given in WW cell coordinate space inside gate pattern '''
         if (port == 'Data0x0'):
-            return (0, 2)
+            return (2, 0)
         elif (port == 'Data1x0'):
-            return (0, 9)
+            return (9, 0)
         elif (port == 'Result0'):
-            return (17, 7)
+            return (7, 17)
+            
+    def get_port_local_tile_pos(self, port):
+        ''' Ports' locations are given in 6-tiles coordinate space local to pattern '''
+        if (port == 'Data0x0'):
+            return (0, 0)
+        elif (port == 'Data1x0'):
+            return (1, 0)
+        elif (port == 'Result0'):
+            return (1, 2)
+            
+    def get_name(self):
+        return self._LPM_instance_name
         
-        
-def instantiate_LPM_AND(size, width):
-    if (size != 2 or width != 1):
-        raise Error('Currently LPM_AND supports only LPM_SIZE=2 and LPM_WIDTH=1')
-    return LPM_AND()
     
