bba225017a01cdbb93be680ce1132f854d73772a
Alexandr Nigay <nigai.alex@gmail.com>
2014-12-22 14:05:32 +0600

    Added LPM_OR.

--- edif2ww/wireworld_lpm_tile6.py
+++ edif2ww/wireworld_lpm_tile6.py
@@ -67,3 +67,64 @@ class LPM_AND:
         return self._LPM_instance_name
         
     
+class LPM_OR:
+    _pattern = [
+        '      ',
+        '      ',
+        'CCC   ',
+        '   C C',
+        '  CCC ',
+        '   C  ',
+        '  C   ',
+        ' C    ',
+        'C     ',
+        '      ',
+        '      ',
+        '      '
+    ]
+    
+    _LPM_instance_name = ''
+    
+    def __init__(self, instance_name, LPM_SIZE, LPM_WIDTH):
+        self._LPM_instance_name = instance_name
+        if (LPM_SIZE != 2 or LPM_WIDTH != 1):
+            raise RuntimeError('Currently LPM_OR supports only LPM_SIZE=2 and LPM_WIDTH=1')
+    
+    def get_pattern(self):
+        return self._pattern
+        
+    def get_delay(self):
+        ''' ... in WW generations '''
+        return 6
+        
+    def get_size_in_tiles(self):
+        ''' Tiles of size 6. Returns tuple (height, width) '''
+        return (2, 1)
+        
+    def get_size_in_cells(self):
+        ''' Returns tuple (height, width) '''
+        return (12, 6)
+        
+    def get_port_local_pos(self, port):
+        ''' 
+            Ports' locations are given in WW cell coordinate space inside gate pattern. 
+            Returns tuple (row, col), 0-based.
+        '''
+        if (port == 'Data0x0'):
+            return (2, 0)
+        elif (port == 'Data1x0'):
+            return (8, 0)
+        elif (port == 'Result0'):
+            return (3, 5)
+            
+    def get_port_local_tile_pos(self, port):
+        ''' Ports' locations are given in 6-tiles coordinate space local to pattern '''
+        if (port == 'Data0x0'):
+            return (0, 0)
+        elif (port == 'Data1x0'):
+            return (1, 0)
+        elif (port == 'Result0'):
+            return (0, 0)
+            
+    def get_name(self):
+        return self._LPM_instance_name
